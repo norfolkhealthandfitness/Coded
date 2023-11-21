d0751b28f9965bf631394981ef19fe7f6ca2f691
joshuaforster <110224330+joshuaforster@users.noreply.github.com>
2023-11-21 10:07:39 +0000

    push

---
+++ counter.js
@@ -0,0 +1,12 @@
+const counterElement = document.getElementById('numberCounter');
+let currentValue = 250; // start value
+const maxValue = 500; // end value
+
+const interval = setInterval(() => {
+    currentValue += 1; // increment by 1, you can adjust this for different speeds
+    counterElement.textContent = currentValue + '+';
+
+    if (currentValue >= maxValue) {
+        clearInterval(interval); // stop the counter when it reaches 500
+    }
+}, 10);
