920495697446827fa698c03f5e052f8a85e26340
joshuaforster <110224330+joshuaforster@users.noreply.github.com>
2023-11-24 09:55:14 +0000

    push

--- header.js
+++ header.js
@@ -1,22 +1,53 @@
-// ======================================
-// Hamburger Menu Section
-// ======================================
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+// Header bar
+const insertHeader = document.querySelectorAll('.insert-header')
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
+insertHeader.forEach(x =>{
+  x.innerHTML = `    
+  <nav class = "navbar"> 
+    <a href="index.html" class="nav-branding"> <img class="logo" src="images/LogoforJolrust.webp"></a>
 
-document.querySelectorAll(".nav-link").forEach(m => m.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}));
+    <ul class="nav-menu">
+        <li class="nav-item">
+            <a href="index.html" class="nav-link">Home</a>
+        </li>
+
+        <li class="nav-item">
+            <a href="about.html" class="nav-link">About</a>
+        </li>
+
+        <li class="nav-item">
+            <a href="services.html" class="nav-link">Services</a>
+        </li>
+
+        <li class="nav-item">
+            <a href="blog.html" class="nav-link">Blog</a>
+        </li>
+
+        <li class="nav-item">
+            <a  href="gallery.html" class="nav-link">Gallery</a>
+        </li>
+
+        <li class="nav-item">
+            <a  href="FAQ.html" class="nav-link">FAQ's</a>
+        </li>
+      
+        <li class="nav-item">
+            <a class="button green" href="contact.html" class="button primary-button nav-link">Contact</a>
+          </li>
+    </ul>
+    <div class="hamburger">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+    </div>
+
+</nav>` 
+})
 
 
 
 
-// ======================================
-// Hamburger Menu End
-// ======================================
+
+
+
+
