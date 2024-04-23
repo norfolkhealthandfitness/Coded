fd5a124bc9cf2c188b1055bdbfa77fc3681ad3ab
joshuaforster <110224330+joshuaforster@users.noreply.github.com>
2024-04-23 14:39:36 +0100

    changes

--- contact.js
+++ contact.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
                         <h2>Get In Touch With Us Today...</h2>
                         <p>If you would like any more information then please do not hesitate to contact us. Please feel free to call, email or send us a message and we will get back to you right away. We will process your data in accordance with our Privacy Notice. You may withdraw this consent at any time by unsubscribing.</p>
                     </div>
-                
+
                     <form class="email-form" method="POST" data-netlify="true" data-netlify-recaptcha="true" name="contactForm">
                         <div class="name-group">
                             <div class="form-group name-box">
@@ -34,7 +34,6 @@ document.addEventListener('DOMContentLoaded', function () {
                             <textarea id="enquiry" name="enquiry" placeholder="Enquiry" required></textarea>
                         </div>
         
-                        <div data-netlify-recaptcha="true"></div>
         
                         <div style='margin: 0 auto' class="buttons-div">
                             <button class="button red" type="submit">Contact</button>
