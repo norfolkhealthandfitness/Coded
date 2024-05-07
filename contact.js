89e36ef080f72a38c1adbff2a328abc2a53786e0
joshuaforster <110224330+joshuaforster@users.noreply.github.com>
2024-05-07 09:28:41 +0100

    changes

--- contact.js
+++ contact.js
@@ -4,11 +4,13 @@ document.addEventListener('DOMContentLoaded', function () {
     if (contact) {
         contact.innerHTML = `
             <div class="contact">
-                <div class="container">
+                <div class="contact-container">
                     <div class="overlay"></div>
-                    <div style='margin-bottom: 24px; margin-top:24px' class="middle-align">
-                        <h2>Get In Touch With Us Today...</h2>
-                        <p>If you would like any more information then please do not hesitate to contact us. Please feel free to call, email or send us a message and we will get back to you right away. We will process your data in accordance with our Privacy Notice. You may withdraw this consent at any time by unsubscribing.</p>
+                    <div style='margin-bottom: 24px; margin-top:24px; text-align: center'>
+                        <h2>I want to talk to someone</h2>
+                        <p>If you have any queries about any of our courses or if you require any further information, please contact us by adding your details below and one of our team will get right back to you. We will process your data in accordance with our Privacy Notice. You may withdraw this consent at any time by unsubscribing.
+
+                        </p>
                     </div>
 
                     <form class="email-form" method="POST" data-netlify="true" data-netlify-recaptcha="true" name="contactForm">
@@ -32,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function () {
                             <textarea id="enquiry" name="enquiry" placeholder="Enquiry" required></textarea>
                         </div>
                         <div data-netlify-recaptcha="true"></div>
-                        <div style='margin: 0 auto' class="buttons-div">
+                        <div style='margin: 0 auto;' class="buttons-div">
                             <button class="button red" type="submit">Contact</button>
                         </div>
                     </form>
