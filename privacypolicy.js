1b01ebf02ada95985284dea7baf56e8ce5ec6108
joshuaforster <110224330+joshuaforster@users.noreply.github.com>
2024-05-13 18:28:40 +0100

    changes

--- privacypolicy.js
+++ privacypolicy.js
@@ -1,26 +1,4 @@
 
-
-// import { documentToHtmlString } from 'https://cdn.skypack.dev/@contentful/rich-text-html-renderer';
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     fetch('http://localhost:8888/.netlify/functions/contentful?content_type=legal')
-//         .then(response => response.json())
-//         .then(data => {
-//             if (data && data.items && data.items.length > 0) {
-//                 const firstDocument = data.items[0];
-//                 const content = firstDocument.fields.content;
-
-//                 const renderedHtml = documentToHtmlString(content);
-//                 document.getElementById('legal-document').innerHTML = `<div>${renderedHtml}</div>`;
-//             } else {
-//                 document.getElementById('legal-document').innerHTML = `<p>No legal documents found.</p>`;
-//             }
-//         })
-//         .catch(error => console.error('Error fetching data:', error));
-// });
-
-
-
 import { documentToHtmlString } from 'https://cdn.skypack.dev/@contentful/rich-text-html-renderer';
 
 document.addEventListener("DOMContentLoaded", function() {
