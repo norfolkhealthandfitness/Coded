af86ae7f2274c42e4512400f3b6e9282ff2e57f4
joshuaforster <110224330+joshuaforster@users.noreply.github.com>
2024-05-14 09:44:52 +0100

    corrected url

--- privacypolicy.js
+++ privacypolicy.js
@@ -1,11 +1,9 @@
-
 import { documentToHtmlString } from 'https://cdn.skypack.dev/@contentful/rich-text-html-renderer';
 
 document.addEventListener("DOMContentLoaded", function() {
     // Determine the base URL based on location.hostname
     const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-    // const baseURL = isLocalhost ? 'http://localhost:8888/.netlify/functions' : 'https://illustrious-twilight-d76498.netlify.app.netlify/functions';
-    const baseURL = isLocalhost ? 'http://localhost:8888/.netlify/functions' : 'https://illustrious-twilight-d76498.netlify.app.netlify/functions';
+    const baseURL = isLocalhost ? 'http://localhost:8888/.netlify/functions' : 'https://illustrious-twilight-d76498.netlify.app/.netlify/functions';
     const url = `${baseURL}/contentful?content_type=legal`;
 
     fetch(url)
