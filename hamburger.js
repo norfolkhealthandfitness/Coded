dd330342bb17b9b3c4eb41aeef2642812cbfb7b1
joshuaforster <110224330+joshuaforster@users.noreply.github.com>
2024-05-15 06:38:21 +0100

    ”changes”

--- hamburger.js
+++ hamburger.js
@@ -2,19 +2,20 @@
 // ======================================
 // Hamburger Menu Section
 // ======================================
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
+
+  document.querySelectorAll(".nav-link").forEach(m => m.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }));
 });
-
-document.querySelectorAll(".nav-link").forEach(m => m.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}));
-
 // ======================================
 // Hamburger Menu End
 // ======================================
